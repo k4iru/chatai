a9cadba5b5a7d1dee78a1bc653f6bd3f51f9d7fb
Kyle Cheung <cheung.kyle1@gmail.com>
2024-12-28 22:57:47 -0500

    added shadcn button

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,8 +14,8 @@ export default function RootLayout({
 }>) {
   return (
     <ClerkProvider>
-      <html lang='en'>
-        <body>{children}</body>
+      <html lang="en">
+        <body className="min-h-screen flex">{children}</body>
       </html>
     </ClerkProvider>
   );
